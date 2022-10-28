4bd3c7a28fa6dd906043c6e79416bf8cdd30a5a5
Vladimir Agafonkin <agafonkin@gmail.com>
2022-10-28 16:29:46 +0300

    upgrade deps and use build-in node test runner

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const output = (input, file, plugins) => ({
     input,
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 
 import fs from 'fs';
-import {test} from 'tape';
+import test from 'node:test';
+import assert from 'node:assert/strict';
 import robustOrientation from 'robust-orientation';
 import nextafter from 'nextafter';
 
@@ -11,10 +12,10 @@ import {
     insphere, inspherefast
 } from '../index.js';
 
-test('orient2d', (t) => {
-    t.ok(orient2d(0, 0, 1, 1, 0, 1) < 0, 'clockwise');
-    t.ok(orient2d(0, 0, 0, 1, 1, 1) > 0, 'counterclockwise');
-    t.ok(orient2d(0, 0, 0.5, 0.5, 1, 1) === 0, 'collinear');
+test('orient2d', () => {
+    assert.ok(orient2d(0, 0, 1, 1, 0, 1) < 0, 'clockwise');
+    assert.ok(orient2d(0, 0, 0, 1, 1, 1) > 0, 'counterclockwise');
+    assert.ok(orient2d(0, 0, 0.5, 0.5, 1, 1) === 0, 'collinear');
 
     const r = 0.95;
     const q = 18;
@@ -30,88 +31,82 @@ test('orient2d', (t) => {
             const o2 = robustOrientation[3]([x, y], [q, q], [p, p]);
 
             if (Math.sign(o) !== Math.sign(o2)) {
-                t.fail(`${x},${y}, ${q},${q}, ${p},${p}: ${o} vs ${o2}`);
+                assert.fail(`${x},${y}, ${q},${q}, ${p},${p}: ${o} vs ${o2}`);
             }
         }
     }
-    t.pass('512x512 near-collinear');
+    // 512x512 near-collinear
 
     const lines = fs.readFileSync(new URL('./fixtures/orient2d.txt', import.meta.url), 'utf8').trim().split(/\r?\n/);
     for (const line of lines) {
         const [, ax, ay, bx, by, cx, cy, sign] = line.split(' ').map(Number);
         const result = orient2d(ax, ay, bx, by, cx, cy);
         if (Math.sign(result) !== -sign) {
-            t.fail(`${line}: ${result} vs ${-sign}`);
+            assert.fail(`${line}: ${result} vs ${-sign}`);
         }
     }
-    t.pass('1000 hard fixtures');
-
-    t.end();
+    // 1000 hard fixtures
 });
 
-test('orient2dfast', (t) => {
-    t.ok(orient2dfast(0, 0, 1, 1, 0, 1) < 0, 'counterclockwise');
-    t.ok(orient2dfast(0, 0, 0, 1, 1, 1) > 0, 'clockwise');
-    t.ok(orient2dfast(0, 0, 0.5, 0.5, 1, 1) === 0, 'collinear');
-    t.end();
+test('orient2dfast', () => {
+    assert.ok(orient2dfast(0, 0, 1, 1, 0, 1) < 0, 'counterclockwise');
+    assert.ok(orient2dfast(0, 0, 0, 1, 1, 1) > 0, 'clockwise');
+    assert.ok(orient2dfast(0, 0, 0.5, 0.5, 1, 1) === 0, 'collinear');
 });
 
-test('incircle', (t) => {
-    t.ok(incircle(0, -1, 0, 1, 1, 0, -0.5, 0) < 0, 'inside');
-    t.ok(incircle(0, -1, 1, 0, 0, 1, -1, 0) === 0, 'on circle');
-    t.ok(incircle(0, -1, 0, 1, 1, 0, -1.5, 0) > 0, 'outside');
+test('incircle', () => {
+    assert.ok(incircle(0, -1, 0, 1, 1, 0, -0.5, 0) < 0, 'inside');
+    assert.ok(incircle(0, -1, 1, 0, 0, 1, -1, 0) === 0, 'on circle');
+    assert.ok(incircle(0, -1, 0, 1, 1, 0, -1.5, 0) > 0, 'outside');
 
     const a = nextafter(-1, 0);
     const b = nextafter(-1, -2);
 
-    t.ok(incircle(1, 0, -1, 0, 0, 1, 0, a) < 0, 'near inside');
-    t.ok(incircle(1, 0, -1, 0, 0, 1, 0, b) > 0, 'near outside');
+    assert.ok(incircle(1, 0, -1, 0, 0, 1, 0, a) < 0, 'near inside');
+    assert.ok(incircle(1, 0, -1, 0, 0, 1, 0, b) > 0, 'near outside');
 
     let x = 1e-64;
     for (let i = 0; i < 128; i++) {
-        if (incircle(0, x, -x, -x, x, -x, 0, 0) <= 0) t.fail(`incircle test ${x}, outside`);
-        if (incircle(0, x, -x, -x, x, -x, 0, 2 * x) >= 0) t.fail(`incircle test ${x}, inside`);
-        if (incircle(0, x, -x, -x, x, -x, 0, x) !== 0) t.fail(`incircle test ${x}, cocircular`);
+        if (incircle(0, x, -x, -x, x, -x, 0, 0) <= 0) assert.fail(`incircle test ${x}, outside`);
+        if (incircle(0, x, -x, -x, x, -x, 0, 2 * x) >= 0) assert.fail(`incircle test ${x}, inside`);
+        if (incircle(0, x, -x, -x, x, -x, 0, x) !== 0) assert.fail(`incircle test ${x}, cocircular`);
         x *= 10;
     }
-    t.pass(`${128 * 3} incircle tests`);
+    // 384 incircle tests
 
     const lines = fs.readFileSync(new URL('./fixtures/incircle.txt', import.meta.url), 'utf8').trim().split(/\r?\n/);
     for (const line of lines) {
         const [, ax, ay, bx, by, cx, cy, dx, dy, sign] = line.split(' ').map(Number);
         const result = incircle(ax, ay, bx, by, cx, cy, dx, dy);
         if (Math.sign(result) !== sign) {
-            t.fail(`${line}: ${result} vs ${sign}`);
+            assert.fail(`${line}: ${result} vs ${sign}`);
         }
     }
-    t.pass('1000 hard fixtures');
-
-    t.end();
+    // 1000 hard fixtures
 });
 
-test('incirclefast', (t) => {
-    t.ok(incirclefast(0, -1, 0, 1, 1, 0, -0.5, 0) < 0, 'inside');
-    t.ok(incirclefast(0, -1, 0, 1, 1, 0, -1, 0) === 0, 'on circle');
-    t.ok(incirclefast(0, -1, 0, 1, 1, 0, -1.5, 0) > 0, 'outside');
-    t.end();
+test('incirclefast', () => {
+    assert.ok(incirclefast(0, -1, 0, 1, 1, 0, -0.5, 0) < 0, 'inside');
+    assert.ok(incirclefast(0, -1, 0, 1, 1, 0, -1, 0) === 0, 'on circle');
+    assert.ok(incirclefast(0, -1, 0, 1, 1, 0, -1.5, 0) > 0, 'outside');
 });
 
-test('orient3d', (t) => {
-    t.ok(orient3d(
+test('orient3d', () => {
+    assert.ok(orient3d(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, 1
     ) > 0, 'above');
 
-    t.ok(orient3d(
+    assert.ok(orient3d(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, -1
     ) < 0, 'below');
 
-    t.ok(orient3d(
+    assert.ok(orient3d(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
@@ -121,14 +116,14 @@ test('orient3d', (t) => {
     const a = nextafter(0, 1);
     const b = nextafter(0, -1);
 
-    t.ok(orient3d(
+    assert.ok(orient3d(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, a
     ) > 0, 'near above');
 
-    t.ok(orient3d(
+    assert.ok(orient3d(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
@@ -139,10 +134,10 @@ test('orient3d', (t) => {
     for (const line of lines) {
         const [, ax, ay, az, bx, by, bz, cx, cy, cz, dx, dy, dz, sign] = line.split(' ').map(Number);
         const result = orient3d(ax, ay, az, bx, by, bz, cx, cy, cz, dx, dy, dz);
-        if (Math.sign(result) !== sign) t.fail(`${line}: ${result} vs ${sign}`);
-        if (Math.sign(result) !== Math.sign(orient3d(dx, dy, dz, bx, by, bz, ax, ay, az, cx, cy, cz))) t.fail('symmetry');
+        if (Math.sign(result) !== sign) assert.fail(`${line}: ${result} vs ${sign}`);
+        if (Math.sign(result) !== Math.sign(orient3d(dx, dy, dz, bx, by, bz, ax, ay, az, cx, cy, cz))) assert.fail('symmetry');
     }
-    t.pass('1000 hard fixtures');
+    // 1000 hard fixtures
 
     const tol = 5.0e-14;
 
@@ -151,41 +146,37 @@ test('orient3d', (t) => {
         const ay = 0.5 + tol * Math.random();
         const az = 0.5 + tol * Math.random();
         const b = 12, c = 24, d = 48;
-        if (orient3d(b, b, b, c, c, c, d, d, d, ax, ay, az) !== 0) t.fail('degenerate');
-        if (orient3d(c, c, c, d, d, d, ax, ay, az, b, b, b) !== 0) t.fail('degenerate');
+        if (orient3d(b, b, b, c, c, c, d, d, d, ax, ay, az) !== 0) assert.fail('degenerate');
+        if (orient3d(c, c, c, d, d, d, ax, ay, az, b, b, b) !== 0) assert.fail('degenerate');
     }
-    t.pass('1000 degenerate cases');
-
-    t.end();
+    // 1000 degenerate cases
 });
 
-test('orient3dfast', (t) => {
-    t.ok(orient3dfast(
+test('orient3dfast', () => {
+    assert.ok(orient3dfast(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, 1
     ) > 0, 'above');
 
-    t.ok(orient3dfast(
+    assert.ok(orient3dfast(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, -1
     ) < 0, 'below');
 
-    t.ok(orient3dfast(
+    assert.ok(orient3dfast(
         0, 0, 0,
         0, 1, 0,
         1, 0, 0,
         0, 0, 0
     ) === 0, 'coplanar');
-
-    t.end();
 });
 
-test('insphere', (t) => {
-    t.ok(insphere(
+test('insphere', () => {
+    assert.ok(insphere(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -193,7 +184,7 @@ test('insphere', (t) => {
         0, 0, 0
     ) < 0, 'inside');
 
-    t.ok(insphere(
+    assert.ok(insphere(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -201,7 +192,7 @@ test('insphere', (t) => {
         0, 0, 2
     ) > 0, 'outside');
 
-    t.ok(insphere(
+    assert.ok(insphere(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -212,7 +203,7 @@ test('insphere', (t) => {
     const a = nextafter(-1, 0);
     const b = nextafter(-1, -2);
 
-    t.ok(insphere(
+    assert.ok(insphere(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -220,7 +211,7 @@ test('insphere', (t) => {
         0, 0, a
     ) < 0, 'near inside');
 
-    t.ok(insphere(
+    assert.ok(insphere(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -233,16 +224,14 @@ test('insphere', (t) => {
         const [, ax, ay, az, bx, by, bz, cx, cy, cz, dx, dy, dz, ex, ey, ez, sign] = line.split(' ').map(Number);
         const result = insphere(ax, ay, az, bx, by, bz, cx, cy, cz, dx, dy, dz, ex, ey, ez);
         if (Math.sign(result) !== -sign) {
-            t.fail(`${line}: ${result} vs ${-sign}`);
+            assert.fail(`${line}: ${result} vs ${-sign}`);
         }
     }
-    t.pass('1000 hard fixtures');
-
-    t.end();
+    // 1000 hard fixtures
 });
 
-test('inspherefast', (t) => {
-    t.ok(inspherefast(
+test('inspherefast', () => {
+    assert.ok(inspherefast(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -250,7 +239,7 @@ test('inspherefast', (t) => {
         0, 0, 0
     ) < 0, 'inside');
 
-    t.ok(inspherefast(
+    assert.ok(inspherefast(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
@@ -258,13 +247,11 @@ test('inspherefast', (t) => {
         0, 0, 2
     ) > 0, 'outside');
 
-    t.ok(inspherefast(
+    assert.ok(inspherefast(
         1, 0, 0,
         0, -1, 0,
         0, 1, 0,
         0, 0, 1,
         0, 0, -1
     ) === 0, 'cospherical');
-
-    t.end();
 });
